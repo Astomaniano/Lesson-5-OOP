2ec86d38d08b6a192ddf9b6c9390b572c69ac49f
Astomaniano <alonso.astomaniano@gmail.com>
2024-04-26 18:43:56 +0400

    add new class

---
+++ main.py
@@ -0,0 +1,29 @@
+class Warrior():
+
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance += 2
+
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power += 1
+
+    def hit(self):
+        print(f"{self.name} бъет кого-то")
+        self.endurance -= 1
+
+    def walk(self):
+        print(f"{self.name} гуляет")
+        self.endurance -= 1
+
+    def info(self):
+        print(f"Имя: {self.name}")
+        print(f"Сила: {self.power}")
+        print(f"Выносливость: {self.endurance}")
+        print(f"Цвет волос: {self.hair_color}")
